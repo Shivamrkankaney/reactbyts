2f49d5516ab54d61203f819c2d21f8df807f06de
sakshi2299 <sakshi.joshi0722@gmail.com>
2023-08-17 12:36:02 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HomeComponent } from './tsproject1/HomeComponent';
+import { Introduction } from './portfolio/pindex';
+
 
 
 
 function App() {
   return (
-   <HomeComponent/>
+   <Introduction/>
   );
 }
 
